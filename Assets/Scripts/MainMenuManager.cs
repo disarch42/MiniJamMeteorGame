4962faf9e9060809b76df5f367dab3ab3e983439
Mehmet Koray Yılmaz <korpik123456@gmail.com>
2025-08-17 11:53:30 +0200

    bush bush

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
- public void StartRound()
+
+    private void Start()
+    {
+        MainCanvas.instance.Show();
+    }
+    public void StartRound()
     {
         // Load the game scene
+        MainCanvas.instance.Hide();
         UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
     }
 }
